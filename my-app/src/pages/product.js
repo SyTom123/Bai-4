57fb56d1f9636b7db374ccef79f73082624752c0
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-08-06 10:55:45 +0700

    refactor: create product db

--- my-app/src/pages/product.js
+++ my-app/src/pages/product.js
@@ -1,9 +1,23 @@
+import { getAll } from "../api/products";
+
 const Product = {
-    render() {
+    async render() {
+        const res = await getAll();
         return /* html */`
-        <div class ="max-w-5xl mx-auto" >
-            <h1>Product Page</h1>
+        <div >
+        <h2 class="font-semibold text-2xl uppercase my-4">Tin tức học tập</h2>
+        <div class="grid grid-cols-3 gap-8">
+            ${res.data.map((item) => `
+            <div class="border p-3">
+                <a href="/news/${item.id}">
+                    <img class=" h-[250px]" src="${item.thumbnail}" alt="" />
+                </a>
+                <h3 class="my-3"><a class="font-semibold text-lg text-orange-500 my-4" href="/news/${item.id}" >${item.title}</a></h3>
+                <p>${item.desc}</p>
+            </div>
+            `).join("")}
         </div>
+    </div>
         `;
     },
 };
